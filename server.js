2370fd190a6678423ae3e0796cb42489aaec5bc9
ksrawr <kenneth.surban62@gmail.com>
2020-03-06 13:36:38 -0800

    sets up mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const db = require('./models');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
