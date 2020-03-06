b3ba341bfcfc7cd616dd81defa62bff15758c192
ksrawr <kenneth.surban62@gmail.com>
2020-03-06 14:50:11 -0800

    serves public folder for rendering views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 const db = require('./models');
@@ -8,6 +9,8 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.sendFile('./views/register.html', {
@@ -21,7 +24,89 @@ app.get('/login', (req, res) => {
 	})
 })
 
-/* User Route */
+
+/* -------------------- AUTH API ROUTES ------------ */
+
+// AUTH Register
+app.post('/api/v1/register', (req, res) => {
+
+	db.User.findOne({email: req.body.email}, (err, foundUser) => {
+
+		if(err) return res.status(500).json({message: "Something went wrong", error});
+
+		if(foundUser) return res.status(409).json({message: "User already registered"});
+
+		bcrypt.genSalt(10, (err, salt) => {
+
+			if(error) return res.status(500).json({message: 'Something went wrong'});
+
+			bcrypt.hash(req.body.password, salt, (error, hash) => {
+
+				if(error) return res.status(500).json({message: 'Something went wrong'});
+
+				const newUser = {
+					name: req.body.name,
+					email: req.body.email,
+					password: hash,
+				};
+
+				db.User.create(newUser, (error, createdUser) => {
+
+					if(error) return res.status(500).json({message: "Something went wrong"});
+
+					res.sendStatus(201).json({message: 'user created'});
+
+				})
+
+			})
+
+		})
+
+	})
+
+})
+
+// AUTH Login
+app.post('/api/v1/login', (req, res) => {
+
+	db.User.findOne({email: req.body.email}, (err, foundUser) => {
+
+		if(err) return res.status(500).json({message: "Something went wrong", err});
+
+		if(!foundUser) return res.status(404).json({message: 'User not registered', err});
+
+		bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+
+			if(err) return res.status(500).json({message: "Something went wrong"});
+
+			if(isMatch) {
+
+				request.session.currentUser = {
+					id: foundUser._id,
+					name: foundUser.name,
+					email: foundUser.email
+				};
+
+				const responseObj = {
+					status: 200,
+					data: foundUser._id,
+					requestedAt: new Date().toLocaleString(),
+					message: 'Success',
+				};
+
+				return res.status(200).json(responseObj);
+			} else {
+				return res.status(400).json({message: 'Username/password is incorrect'});
+			}
+
+		})
+
+	})
+
+})
+
+
+/* User Index Route */
 app.get('/api/v1/users', (req, res) => {
 
 	db.User.find({}, (err, foundUsers) => {
