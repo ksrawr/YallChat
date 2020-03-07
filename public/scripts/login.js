891bfbf61d8c13d2989173dfb85288245c1c7837
ksrawr <kenneth.surban62@gmail.com>
2020-03-06 18:03:59 -0800

    completes functionality for auth on client side

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,1 +1,35 @@
 console.log(' i am login js');
+
+const formEl = document.querySelector('form');
+	
+const handleSubmit = () => {
+	event.preventDefault();
+
+	let formIsValid = true;
+	const userData = {};
+
+	/* pull all the form's children */
+ 	const formInputs = [...formEl.elements];
+
+ 	formInputs.forEach(input => {
+ 		if(input.type !== 'submit') userData[input.name] = input.value;
+ 	});
+
+ 	console.log(userData);
+
+ 	fetch('/api/v1/login', {
+ 		method: 'POST',
+ 		headers: {
+ 			'Content-Type': 'application/json',
+ 		},
+ 		body: JSON.stringify(userData)
+ 	})
+ 		.then(res => res.json())
+ 		.then(data => {
+ 			console.log(data);
+ 			if(data.status === 200) window.location = '/home';
+ 		})
+ 		.catch(err => console.warn(err));
+}
+
+formEl.addEventListener('submit', handleSubmit);
