fc73b82a333370601bc9358a015772a4f98fda79
ksrawr <kenneth.surban62@gmail.com>
2020-03-10 15:02:37 -0700

    adds display all chats current user has created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,10 +6,44 @@ let state = {
 };
 
 const userNameEl = document.getElementById('userName');
+const navChatEl = document.getElementById('nav-chat');
+
+const displayChats = () => {
+	return state.chatrooms.map(chatroom => {
+		return `
+			<div class="card">
+				<div class="card-body">
+					<div class="d-flex justify-content-between align-items-center">
+				  	<p><strong class='chat-name'>${chatroom.name}</strong></p>
+				  	<p><span class='chat-time'>11:20pm</span></p>
+				  </div>
+				  <p>Display last message.</p>
+				</div>
+
+			</div>
+		`
+	}).join('');
+}
 
 const render = () => {
 	userNameEl.innerHTML = '';
 	userNameEl.innerHTML = `${state.username}!`;
+
+	navChatEl.innerHTML = `
+		<div class="card search-container container">
+			<div class="card-body">
+				<form class="form-inline" id="searchChat">
+				  <div class="form-group mb-2 mx-sm-1">
+				    <input type="text" class="form-control" id="chatQuery" placeholder="Search Chats">
+				    <button type="submit" class="btn btn-primary">Search</button>
+				  </div>
+				</form>
+			</div>
+		</div>`;
+	
+	if(state.chatrooms.length > 0) {
+		navChatEl.insertAdjacentHTML('beforeend', displayChats());
+	}
 }
 
 const setState = (obj) => {
