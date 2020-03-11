3ead0a4769874874260bccddef40a90680f62c6d
ksrawr <kenneth.surban62@gmail.com>
2020-03-10 22:57:52 -0700

    renders messages from selected chatroom

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ let state = {
 	username: '',
 	chatrooms: [],
 	currentChat: null,
+	messages: []
 };
 
 const userNameEl = document.getElementById('userName');
@@ -24,6 +25,24 @@ const displayChats = () => {
 			</div>
 		`
 	}).join('');
+}
+
+const displayMessages = () => {
+	return state.messages.map((message, index) => {
+		if(message.author === localStorage.getItem('uid')) {
+			return `
+				<div class="message-user">
+					${message.content}
+				</div>
+			`;
+		} else {
+			return `
+				<div class="message-other">
+					${message.content}
+				</div>
+			`;
+		}
+	}).join('')
 }
 
 const handleChatClick = () => {
@@ -60,12 +79,47 @@ const handleChatClick = () => {
 			.then(res => res.json())
 			.then(data => {
 				console.log(data);
+				setState({
+					currentChat: data.data,
+					messages: data.data.messages.slice(-5)
+				});
 			})
 			.catch(err => console.warn(err));
 	} else {
 		console.log({id});
 	}
 
+}
+
+const handleSubmitMessage = () => {
+	event.preventDefault();
+
+	console.log('i am handle submit message');
+
+	const messageInput = document.getElementById('messageInput')
+
+	if(messageInput) {
+
+		const messageObj = {
+			content: messageInput.value,
+			author: localStorage.getItem('uid')
+		};
+
+		fetch(`/api/v1/chatrooms/${state.currentChat._id}/messsages`, {
+			method: 'PUT',
+			headers: {
+	 	 		'Content-Type': 'application/json',
+	 	 	},
+	 	 	body: JSON.stringify(messageObj)
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				state.messages.push(data.data.messages.slice(-1).shift());
+				render();
+			})
+			.catch(err => console.log(err))
+	}
 }
 
 const render = () => {
@@ -88,6 +142,49 @@ const render = () => {
 		navChatEl.insertAdjacentHTML('beforeend', displayChats());
 
 		document.querySelectorAll('.card').forEach(chatroom => chatroom.addEventListener('click', handleChatClick));
+	}
+
+	if(state.currentChat) {
+		const { name, date } = state.currentChat;
+		const chatHeaderEl = document.getElementById('chatHeader');
+		const messageStateEl = document.getElementById('messageState');
+		const messageFormEl = document.getElementById('messageForm');
+		const messageListEl = document.getElementById('messageList');
+
+		chatHeaderEl.innerHTML = '';
+
+		chatHeaderEl.innerHTML = `
+			<div class="row">	
+				<div class="col-sm-10">
+					<h4>${name}</h4>
+					<p>${date}</p>
+				</div>
+				<div class="col-sm-2">
+					<button class="btn btn-primary">Settings</button>
+				</div>
+			</div>
+		`;
+
+		messageStateEl.innerHTML = '';
+		messageFormEl.innerHTML = '';
+
+		messageFormEl.innerHTML = `
+		<form id="form-message">
+			<div class="row">
+				<div class="col-sm-10">
+					<input id="messageInput" type="text" name="" placeholder="Message...">
+				</div>
+				<div class="col-sm-2">
+					<button type="submit" class="btn btn-primary">Send</button>
+				</div>
+			</div>
+		</form>
+		`;
+
+		document.getElementById('form-message').addEventListener('submit', handleSubmitMessage);
+
+		messageListEl.innerHTML = '';
+		messageListEl.insertAdjacentHTML('afterbegin', displayMessages());
 	}
 }
 
